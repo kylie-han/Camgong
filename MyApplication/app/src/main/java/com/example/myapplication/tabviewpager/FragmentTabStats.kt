26486225237d352f2158531e294427c83c8a7da8
오유민 <dbals75@naver.com>
2020-10-30 16:04:39 +0900

    Update date picker dialog

--- MyApplication/app/src/main/java/com/example/myapplication/tabviewpager/FragmentTabStats.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/tabviewpager/FragmentTabStats.kt
@@ -1,22 +1,59 @@
 package com.example.myapplication.tabviewpager
 
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.myapplication.R
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.layout_stats.*
+import kotlinx.android.synthetic.main.layout_stats.view.*
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class FragmentTabStats : Fragment() {
+    var calendar: Calendar = Calendar.getInstance()
+    val year = calendar.get(Calendar.YEAR)
+    val month = calendar.get(Calendar.MONTH) +1
+    val date = calendar.get(Calendar.DATE)
+
+    var myDatePicker =
+        OnDateSetListener { view, year, month, dayOfMonth ->
+            calendar.set(Calendar.YEAR, year)
+            calendar.set(Calendar.MONTH, month)
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateLabel()
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         val view = inflater.inflate(R.layout.layout_stats, container, false)
 
+        view.calendarText.text = "$year.$month.$date"
+
+        val tv_date = view.calendarText
+        tv_date.setOnClickListener {
+            context?.let { it1 ->
+                DatePickerDialog(
+                    it1, R.style.DatePickerTheme,
+                    myDatePicker,
+                    calendar[Calendar.YEAR],
+                    calendar[Calendar.MONTH],
+                    calendar[Calendar.DAY_OF_MONTH]
+                ).show()
+            }
+        }
 
         return view
+    }
+    private fun updateLabel() {
+        val myFormat = "yyyy.MM.dd"
+        val sdf = SimpleDateFormat(myFormat, Locale.KOREA)
+        calendarText.text = sdf.format(calendar.time)
     }
 }
