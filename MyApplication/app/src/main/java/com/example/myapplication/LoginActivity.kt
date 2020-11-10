0c2a46618d069a2d0f32c87a55b028fd70ddee99
taesang <xotkd0709@naver.com>
2020-11-10 10:53:28 +0900

    login exception handling + layout optimization

--- MyApplication/app/src/main/java/com/example/myapplication/LoginActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -39,6 +39,13 @@ class LoginActivity : AppCompatActivity() {
             startActivityForResult(signInIntent, RC_SIGN_IN)
 
         }
+        //로그인 정보가 있는지 확인하고 있으면 바로 메인페이지로
+        if (firebaseAuth!!.getCurrentUser() != null) {
+            // User is signed in (getCurrentUser() will be null if not signed in)
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 
 
