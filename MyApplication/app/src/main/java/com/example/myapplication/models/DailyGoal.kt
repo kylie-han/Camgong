aeda7ebdb87459817d5ec9ef49eacce0f848e6ee
오유민 <dbals75@naver.com>
2020-11-16 12:50:03 +0900

    Refactoring and build apk

--- MyApplication/app/src/main/java/com/example/myapplication/models/DailyGoal.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication.models
-
-import com.google.firebase.database.IgnoreExtraProperties
-
-@IgnoreExtraProperties
-data class DailyGoal(
-    var goalStatus: Boolean = false,
-    var goalTime: Long = 0L
-)
